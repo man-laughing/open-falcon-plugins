b9f77f2189950360c8586df7c9a902d4e23fb831
man-laughing <305835227@qq.com>
2015-07-07 17:09:39 +0800

    Signed-off-by: man-laughing <305835227@qq.com>

---
+++ output.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+from __future__ import division
+import os
+import time
+
+r=open('/sys/class/net/bond0/statistics/tx_bytes','r')
+r_int=int(r.read())
+time.sleep(1)
+rr=open('/sys/class/net/bond0/statistics/tx_bytes','r')
+rr_int=int(rr.read())
+input_flux =  (rr_int - r_int)  * 8 / 1024 / 1024
+
+input_flux_new=str("%.2f"%input_flux)
+hostt=os.getenv('HOSTNAME')
+timee=time.time()
+aaa = [{'endpoint':hostt,'tags':'','timestamp':timee,'metric':'net.flux.eth0','value':input_flux_new,'counterType':'GAUGE','step':60}]
+print aaa
